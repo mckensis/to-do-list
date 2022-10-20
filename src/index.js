d0d6d7bc868ec9ab8dbae8520756c35ec0b8c998
Aidan <adn.mck@gmail.com>
2022-10-20 15:55:54 +0100

    more styling

--- src/index.js
+++ src/index.js
@@ -1,48 +1,11 @@
 import './style.css';
 import { format, formatDistance, formatRelative, subDays } from 'date-fns';
-import createDefaultList from './createDefaultList.js';
-import createDOMHeader from './createDOMHeader.js';
-import createDOMSidebar from './createDOMSidebar.js';
-import createDOMActiveListSection from './createDOMActiveListSection.js';
+import createDOM from './createDOM.js';
 
 //import data from './data.json';
 
 window.addEventListener("load", buildMainPage());
 
-
 function buildMainPage() {
-    
-    const page = document.querySelector('.mainPage');
-    
-    //const listArray = createDefaultLists();
-    
-    page.appendChild(createDOMHeader());
-    page.appendChild(createDOMSidebar());
-    page.appendChild(createDOMActiveListSection());
-    
+    createDOM();
 }
-
-
-
-
-
-
-
-
-
-/*
-function displayActiveList() {
-
-    const list = document.createElement('ul');
-
-    for (const listItem of active.items) {
-        const item = document.createElement('li');
-        item.textContent = listItem;
-        list.appendChild(item);
-    }
-
-    list.classList.add('activeListDisplay');
-
-    return list;
-}
-*/
